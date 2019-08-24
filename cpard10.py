38ec3f56159cef8aa42692de6ac176e73bc2dac1
Tilman Stehr <tilman@tilman.ninja>
2019-08-24 19:46:16 +0200

    Convert writeTo to send data as chunks, allow to specify encoding

--- cpard10.py
+++ cpard10.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python
+import re
+import time
+from textwrap import wrap
 
 import serial
-import re
 
-def __wL(ser, string, encoding = 'utf-8'):
+
+def __wL(ser, string):
     print('sending', string)
     n = ser.write(string + b'\r\n')
     print('sent', n, 'bytes')
 
-def writeTo(outputPath, string, device='/dev/ttyACM0', baud=115200):
-    escapedString = str(string.encode())
-    print(escapedString)
+def writeTo(outputPath, string, device='/dev/ttyACM0', baud=115200, encoding='utf-8'):
+
     ser = serial.Serial(device, baud, timeout=2)
     ser.write(b'\x03')
 
     __wL(ser, bytes('f = open("{}", "w")'.format(outputPath), 'utf-8'))
-    __wL(ser, bytes('f.write({})'.format(escapedString), 'utf-8'))
+    for chunk in wrap(string, 512):
+        time.sleep(0.1)
+        escapedChunk = str(chunk.encode(encoding=encoding))
+        __wL(ser, bytes('f.write({})'.format(escapedChunk), 'utf-8'))
     __wL(ser, b'f.close()')
     ser.close()
 
-def readFrom(inputPath, device='/dev/ttyACM0', baud=115200):
+def readFrom(inputPath, device='/dev/ttyACM0', baud=115200, encoding='utf-8'):
     ser = serial.Serial(device, baud, timeout=2)
     print(ser.getSettingsDict())
     ser.write(b'\x03')
@@ -30,16 +35,17 @@ def readFrom(inputPath, device='/dev/ttyACM0', baud=115200):
 
     ser.flushInput()
 
-    __wL(ser, b'print("<\\x43ARD10DATA>" + data + "</\\x43ARD10DATA>")')
+    # spaces are necessary, otherwise end tag can get corrupted
+    __wL(ser, b'print("<\\x43ARD10DATA>" + data +      "</\\x43ARD10DATA>")')
 
 
-    data = ser.read_until('</CARD10DATA>')
+    data = ser.read_until('</CARD10DATA>\r\n')
     print(len(data))
     print(data)
 
     ser.close()
 
-    match = re.search('<CARD10DATA>(.*)</CARD10DATA>', data.decode(),
+    match = re.search('<CARD10DATA>(.*)</CARD10DATA>', data.decode(encoding=encoding),
       re.DOTALL)
 
     return match.group(1) if match else None
